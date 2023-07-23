1e858cd2d1ee1f2d0c8cb3a8eb21aa0211484fdd
Ashish kumar palai <ashishkumarpalai2000@gmail.com>
2023-07-23 19:28:45 +0530

    pip update

--- backend/app.py
+++ backend/app.py
@@ -489,8 +489,7 @@ def delete_participant_from_event(event_id, participant_id):
     return jsonify({'message': 'Participant removed from the event'}), 200
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
